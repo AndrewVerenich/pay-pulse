6b3799679994ee41bcb497aa7ef63631221916c4
andrey.verenich <andrey.verenich@moneyman.ru>
2026-08-24 10:44:43 +0300

    docker compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,13 +19,16 @@ allprojects {
 
 subprojects {
   plugins.withId("org.jetbrains.kotlin.jvm") {
-    extensions.configure<org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension> {
-      jvmToolchain(21)
-    }
-    tasks.withType<KotlinCompile>().configureEach {
-      compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_21)
-        freeCompilerArgs.addAll("-Xjsr305=strict", "-Xjvm-default=all")
+    // flink-payment-intelligence ограничен JVM 11 (Flink 1.17.x) и конфигурируется в своём build-скрипте.
+    if (name != "flink-payment-intelligence") {
+      extensions.configure<org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension> {
+        jvmToolchain(21)
+      }
+      tasks.withType<KotlinCompile>().configureEach {
+        compilerOptions {
+          jvmTarget.set(JvmTarget.JVM_21)
+          freeCompilerArgs.addAll("-Xjsr305=strict", "-Xjvm-default=all")
+        }
       }
     }
     tasks.withType<Test>().configureEach {
