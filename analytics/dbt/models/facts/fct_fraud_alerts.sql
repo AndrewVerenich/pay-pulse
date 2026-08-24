d9fc164b9420301f1cc0e84aa84afe524acaf462
andrey.verenich <andrey.verenich@moneyman.ru>
2026-08-24 16:42:01 +0300

    dbt fix

--- analytics/dbt/models/facts/fct_fraud_alerts.sql
+++ analytics/dbt/models/facts/fct_fraud_alerts.sql
@@ -12,17 +12,17 @@ with alerts_sk as (
   from {{ ref('stg_fraud_alerts') }}
 )
 select
-  al.alert_sk,
-  al.alert_id,
-  al.user_id,
-  al.payment_id,
-  al.rule_id,
-  al.score,
-  al.reasons,
-  al.alert_date,
-  al.occurred_at,
-  a.account_sk,
-  r.rule_sk
+  al.alert_sk as alert_sk,
+  al.alert_id as alert_id,
+  al.user_id as user_id,
+  al.payment_id as payment_id,
+  al.rule_id as rule_id,
+  al.score as score,
+  al.reasons as reasons,
+  al.alert_date as alert_date,
+  al.occurred_at as occurred_at,
+  a.account_sk as account_sk,
+  r.rule_sk as rule_sk
 from alerts_sk as al
 left join {{ ref('dim_account') }} as a on al.user_id = a.account_id
 left join {{ ref('dim_rule') }} as r on al.rule_id = r.rule_id
--- analytics/dbt/models/facts/fct_payments.sql
+++ analytics/dbt/models/facts/fct_payments.sql
@@ -12,18 +12,18 @@ with payments_sk as (
   from {{ ref('stg_payment_events') }}
 )
 select
-  p.payment_sk,
-  p.event_id,
-  p.payment_id,
-  p.account_id,
-  p.merchant_id,
-  p.currency,
-  p.amount,
-  p.occurred_at,
-  p.event_date,
-  a.account_sk,
-  m.merchant_sk,
-  c.currency_sk
+  p.payment_sk as payment_sk,
+  p.event_id as event_id,
+  p.payment_id as payment_id,
+  p.account_id as account_id,
+  p.merchant_id as merchant_id,
+  p.currency as currency,
+  p.amount as amount,
+  p.occurred_at as occurred_at,
+  p.event_date as event_date,
+  a.account_sk as account_sk,
+  m.merchant_sk as merchant_sk,
+  c.currency_sk as currency_sk
 from payments_sk as p
 inner join {{ ref('dim_account') }} as a on p.account_id = a.account_id
 left join {{ ref('dim_merchant') }} as m on p.merchant_id = m.merchant_id
